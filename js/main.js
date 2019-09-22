dc00b381f563c92b9c4979a56360dd29db413d09
isthatchais <cha96001@byui.edu>
2019-09-21 18:06:31 -0600

    again

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ var options = {
     weekday: "long",
     day: "numeric",
     month: "long",
-    year: "numeric"
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
 };
-document.getElementById('outputDiv').innerHTML = new Date(document.lastModified).toLocaleDateString("en-US",options);
+document.getElementById('outputDiv').innerHTML = new Date(document.lastModified).toLocaleDateString("en-US", options);
 document.getElementById('date').innerHTML = new Date(document.lastModified).getFullYear();
