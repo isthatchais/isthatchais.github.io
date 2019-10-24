876ff65bad73f5a773bd8b5f84ab247a38313bb0
isthatchais <cha96001@byui.edu>
2019-10-24 00:26:48 -0600

    6

---
+++ Week 6/js/preston-6.js
@@ -0,0 +1,34 @@
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+}
+document.getElementById('output').innerHTML = new Date().toLocaleDateString("en-US", options);
+function pancakes() {
+    var dayOfWeek = new Date().getDay();
+    if (dayOfWeek == 5){
+        document.getElementById("pancakes").style.display = "block";
+    }
+    else{
+      document.getElementById("pancakes").style.display = "none";
+    }
+  }
+  function doWindChill() {
+    var temp = parseFloat(document.getElementById("currentTemp").textContent);
+    var speed = parseFloat(document.getElementById("windSpeed").textContent);
+    if (temp <= 50 && speed >= 3){
+        var chill = windChill(temp, speed);
+        document.getElementById('windChill').innerHTML = Math.round(chill);
+    }
+    else{
+        document.getElementById('windChill').innerHTML = "N/A";
+    }
+}
+function windChill(tempF, speed) {
+    var result = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+    return result;
+}
