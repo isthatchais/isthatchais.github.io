b178091d30b75ffa03fdde3c9994c0d523081567
isthatchais <cha96001@byui.edu>
2019-12-12 19:42:03 -0700

    more

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -1,18 +1,21 @@
-fetch("data/templedata.json")
-.then(function (response) {
-  return response.json();
-})
-.then(function (jsonObject) {
-  const prophets = jsonObject["temples"];
+const requestURL = 'https://isthatchais.github.io/final-project/data/templedata.json';
+fetch(requestURL)
+.then((response) => response.json())
+
+.then((jsonObject) => {
+  const temples = jsonObject["temples"];
+
   for (let i = 0; i < temples.length; i++) {
-      if(name === "billings"){
+      if(temples[i].name == "Billings"){
+        let close = temples[i].close;
+        for (let j = 0; j < events.length; j++){
           let billingsclose = document.createElement("section");
           let billingsthisYear = document.createElement("p");
           let billingsnextYear = document.createElement("p");
           billingsthisYear.textContent = temples[i].closures2019;
           billingsnextYear.textContent = temples[i].closures2020;
-          bllingsclose.appendChild(billingsthisYear);
-          bllingsclose.appendChild(billingsnextYear);
+          billingsclose.appendChild(billingsthisYear);
+          billingsclose.appendChild(billingsnextYear);
           document.querySelector('div.billingsclose').appendChild(billingsclose);
       }
       
