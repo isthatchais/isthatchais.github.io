3c410d5b9e32340754be187d52e20283e1e6e698
isthatchais <cha96001@byui.edu>
2019-12-12 21:53:36 -0700

    more

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -6,18 +6,45 @@ fetch(requestURL)
   const temples = jsonObject["temples"];
 
   for (let i = 0; i < temples.length; i++) {
-      if(temples[i].name == "Billings"){
+      if(temples[i].name === "Billings"){
         let close = temples[i].close;
-        for (let j = 0; j < events.length; j++){
-          let billingsclose = document.createElement("section");
-          let billingsthisYear = document.createElement("p");
-          let billingsnextYear = document.createElement("p");
-          billingsthisYear.textContent = temples[i].closures2019;
-          billingsnextYear.textContent = temples[i].closures2020;
-          billingsclose.appendChild(billingsthisYear);
-          billingsclose.appendChild(billingsnextYear);
+        let billingsclose = document.createElement("section");
+        for (let j = 0; j < close.length; j++){
+          let closed = document.createElement("p");
+          closed.textContent = close[j];
+          billingsclose.appendChild(closed);
           document.querySelector('div.billingsclose').appendChild(billingsclose);
-      }
-      
+        }
+    }
+      if(temples[i].name === "Bismarck"){
+        let close = temples[i].close;
+        let bismarckclose = document.createElement("section");
+        for (let j = 0; j < close.length; j++){
+          let closed = document.createElement("p");
+          closed.textContent = close[j];
+          bismarckclose.appendChild(closed);
+          document.querySelector('div.bismarckclose').appendChild(bismarckclose);
+        }
+    }
+    /*if(temples[i].name === "Portland"){
+        let close = temples[i].close;
+        let portlandclose = document.createElement("section");
+        for (let j = 0; j < close.length; j++){
+          let closed = document.createElement("p");
+          closed.textContent = close[j];
+          portlandclose.appendChild(closed);
+          document.querySelector('div.portlandclose').appendChild(portlandclose);
+        }
+    }
+    if(temples[i].name === "St. Paul"){
+        let close = temples[i].close;
+        let stpaulclose = document.createElement("section");
+        for (let j = 0; j < close.length; j++){
+          let closed = document.createElement("p");
+          closed.textContent = close[j];
+          stpaulclose.appendChild(closed);
+          document.querySelector('div.stpaulclose').appendChild(stpaulclose);
+        }
+    }*/
 }
 });
